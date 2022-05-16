50e1fbd60e9220b02c823a3ff319b082367e4297
Aimen Sahnoun <aimen.sahnoun@lyntlabs.com>
2022-05-16 09:55:26 +0300

    🚀 Feature: Finished custom bottom navigation

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+//React Native import
+import { View, Text } from "react-native";
+
 //Dependencies import
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { RecoilRoot } from "recoil";
@@ -15,6 +18,10 @@ import {
 //Pages import
 import UsersPage from "./src/screens/users";
 import PostsPage from "./src/screens/posts";
+
+//Assets import
+import Users from "./src/assets/icons/users-alt.svg";
+import Posts from "./src/assets/icons/postcard.svg";
 
 export default function App() {
   return (
@@ -36,10 +43,63 @@ function MyApp() {
       <Tab.Navigator
         screenOptions={{
           headerShown: false,
+          tabBarShowLabel: false,
+
+          tabBarStyle: [
+            {
+              position: "absolute",
+              left: 60,
+              right: 60,
+              bottom: 20,
+              borderRadius: 15,
+              height: 70,
+            },
+            null,
+          ],
         }}
       >
-        <Tab.Screen name="Users" component={UsersPage} />
-        <Tab.Screen name="Posts" component={PostsPage} />
+        <Tab.Screen
+          name="Users"
+          component={UsersPage}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Users
+                  width={25}
+                  height={25}
+                  fill={focused ? "#FFF" : "#A0A0A0"}
+                />
+                <Text style={{ color: "#000" }}>Users</Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Posts"
+          component={PostsPage}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <Posts
+                  width={25}
+                  height={25}
+                  fill={focused ? "#FFF" : "#A0A0A0"}
+                />
+                <Text style={{ color: "#000" }}>Posts</Text>
+              </View>
+            ),
+          }}
+        />
       </Tab.Navigator>
     </SafeAreaView>
   );
